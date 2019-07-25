cf3635fa150f66dc51b00998545bb7ff5a84ce7e
Maggie Jo Saylor <saylor.mj@gmail.com>
2019-07-24 21:09:22 -0500

    added createreactexpress folder

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+class App extends Component {
+
+  state = {
+    message: "Front End Message"
+  }
+
+  componentDidMount() {
+    axios.get("/testing")
+      .then(response => {
+        this.setState({
+          message: response.data
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
