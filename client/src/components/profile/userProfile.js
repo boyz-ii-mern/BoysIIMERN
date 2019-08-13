104b61930831028539e0fb3f360dae4f287e1091
Daniel <daniel.ryuichi.wyatt@gmail.com>
2019-08-12 19:46:02 -0500

    deleted some useless code from userProfile

--- client/src/components/profile/userProfile.js
+++ client/src/components/profile/userProfile.js
@@ -10,13 +10,7 @@ class UserProfile extends Component {
                     
                     <div className=" card section profile-card valign-wrapper">
                         <div className="profile-section user-section valign-wrapper">
-                            {/* update source to database */}
-                            {/* <IdentityContext.Provider value={{
-        user: this.state.user,
-        loggedIn: this.state.loggedIn
-    }}>
-    </IdentityContext.Provider> */}
-                            {/* <img src="../Images/avatar-01.png" alt="" className="circle responsive-img profile-image" /> */}
+                            
                             <div className="section">
                                 {/* update username to database */}
 
@@ -24,12 +18,8 @@ class UserProfile extends Component {
 
                                 <h4>{loggedIn ? `${user.firstName}` : `Username`}</h4>
 
-                                {/* <h4>User Name</h4> */}
-                                {/* update superlative to database */}
-
                                 <p>{loggedIn ? `${user.superlative}` : `Most Likely to Do Something`}</p>
 
-                                {/* <p>Most Likely to Do Something</p> */}
                             </div>
                         </div>
                     </div>
